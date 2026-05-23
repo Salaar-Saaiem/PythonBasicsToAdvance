9fdbd8231a45149ce0940f1a0e07e442cd5f688a
SaaiemSalaar <saaiem.221846.ci@mhssce.ac.in>
2026-05-23 19:06:28 +0530

    Updated project files

---
+++ 5-ControlStatement/Loops/for Loop/Print even numbers between 1 and 20 using for loop.py
@@ -0,0 +1,13 @@
+'''
+Print even numbers between 1 and 20 using for loop
+'''
+
+for i in range(2,21,2):
+    print(i, end=" ")
+print('\n')
+
+'''
+Print odd numbers between 1 and 20 using for loop
+'''
+for j in range(1,21,2):
+    print (j, end=" ")
---
+++ 5-ControlStatement/Loops/for Loop/Print numbers from 1 to 10 using for loop.py
@@ -0,0 +1,6 @@
+'''
+1. Write a program to print numbers from 1 to 10 using for loop.
+'''
+
+for i in range(1,11): # range function is used to print (start, end) n numbers of values
+    print(i, end=" ") #end=" " prevents entering new line and add space in the same 
