1fae56ee51ea4752fd2c82bf645242434630798f
SaaiemSalaar <saaiem.221846.ci@mhssce.ac.in>
2026-05-23 23:13:08 +0530

    Updated project files

---
+++ 6-Strings/Join Operation.py
@@ -0,0 +1,9 @@
+"""
+''.join(str) operation is used to regroup slpitted items
+"""
+
+names=['sam','saaiem','salaar']
+print(''.join(names))
+print(' '.join(names))
+print('\n'.join(names))
+print(','.join(names))
