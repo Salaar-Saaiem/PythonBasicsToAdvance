ef67b0aa22300c2d04edf273435f0eccb6c4068f
SaaiemSalaar <saaiem.221846.ci@mhssce.ac.in>
2026-05-23 22:41:01 +0530

    Updated project files

---
+++ 6-Strings/MembershipOperation.py
@@ -0,0 +1,11 @@
+'''
+if u want to check whether the word is in the dictionary or not
+function: 
+in -> finds the word in boolean (true/false)
+not in -> reverses the answer
+'''
+
+str= "Saaiem Salaar is king of Pyhton"
+print('salaar' in str)
+print('Salaar' in str)
+print('Salaar' not in str)
