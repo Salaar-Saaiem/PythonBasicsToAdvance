0cd3b2eb3cb790f8d0e451fe7885ca7e4756712f
SaaiemSalaar <saaiem.221846.ci@mhssce.ac.in>
2026-05-23 23:06:15 +0530

    Updated project files

---
+++ 6-Strings/SplittingOperation.py
@@ -0,0 +1,12 @@
+'''
+if we want to split string's words individually, we can use .split()
+OR
+'''
+str='Saaiem Salaar will be the biggest celebrity the world has ever seen'
+print(str.split())
+
+'''
+if we wanna take multiple names from the user and then list each as singles
+'''
+names=input('Enter the Names of the Students: ').split()
+print(names)
