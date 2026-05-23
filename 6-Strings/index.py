dbb9a9c455a9616b13a30af7cdac594ed90dabed
SaaiemSalaar <saaiem.221846.ci@mhssce.ac.in>
2026-05-23 21:43:11 +0530

    Updated project files

---
+++ 6-Strings/index.py
@@ -0,0 +1,10 @@
+'''every string character is indexed and can be accessed individually, spaces are also counted as characters!'''
+
+string='Michael Jackson'
+print(string)
+
+print (string[3]) #it will print H
+print (string[-1]) # it will print last letter which is N
+print('\n')
+for i in string:
+    print (i)
