9b620d8214bf33b4de7d0e34e4581b37e0d2cfc0
SaaiemSalaar <saaiem.221846.ci@mhssce.ac.in>
2026-05-23 18:05:08 +0530

    Updated project files

---
+++ 5-ControlStatement/Loops/While Loop/Display 1 to 5 numbers using while loop.py
@@ -0,0 +1,7 @@
+'''
+Display 1 to 5 numbers using while loop
+'''
+i=1
+while i<=5:
+    print(i)
+    i+=1
---
+++ 5-ControlStatement/Loops/While Loop/Print Even Numbers from 1 to 20.py
@@ -0,0 +1,9 @@
+'''
+2. Write a program to print even numbers from 1 to 20 using while loop.
+'''
+
+i=1
+while i<=20:
+    if i % 2 ==0:
+        print(i)
+    i+=1
