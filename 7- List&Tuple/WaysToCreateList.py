d826c14f5acc73281417cae96fc54f9375ce9ab5
SaaiemSalaar <saaiem.221846.ci@mhssce.ac.in>
2026-06-02 13:27:55 +0530

    Updates on 2nd June 2026

--- 7- List&Tuple/WaysToCreateList.py
+++ 7- List&Tuple/WaysToCreateList.py
@@ -1,7 +1,11 @@
-'''using Square Braces'''
+'''
+using Square Braces
+'''
 my_list= ['saaiem', 18, 'A+', 183.4]
 print(my_list)
 
-'''using constructor list method'''
+'''
+using constructor list method
+'''
 my_list=list(('SAM', 21, 'A-', 190.5)) # double curly braces is medatory
 print(my_list)
