42401f490f9f85e03fe2111be7d9bcdb6fd93c9a
SaaiemSalaar <saaiem.221846.ci@mhssce.ac.in>
2026-06-02 21:54:59 +0530

    Uploaded on 2nd June 2026

---
+++ 10 - OOPS/Prob4.py
@@ -0,0 +1,38 @@
+'''
+4. Bank Account - Create a class BankAccount.
+
+Attributes:
+account_holder
+balance
+
+Methods:
+deposit(amount)
+withdraw(amount)
+check_balance()
+
+Example:
+Deposited ₹500
+Current Balance: ₹1500
+'''
+
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.acc_holder = account_holder
+        self.balance = balance
+    
+    def deposit(self, amount):
+        self.balance += amount
+        print(f'\nDeposited - {amount}')
+
+    def withdraw(self, amount):
+        self.balance -+ amount
+        print(f'\nWithdrawn - {amount}')
+    
+    def check_balance(self):
+        print(f'Current Balance - {self.balance}')
+        
+
+Saaiem=BankAccount('Saaiem', 500000)
+Saaiem.deposit(50000)
+Saaiem.check_balance()
+
